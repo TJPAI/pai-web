2e12cc72421b11de6818918e17d0db1695200ed7
TJPAI <erwu.liu@gmail.com>
2026-09-18 18:36:42 +0800

    chore: refresh cache after history navigation fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pai-site-v20260918-23';
+const CACHE='pai-site-v20260918-24';
 const CORE=[
   './','./index.html','./about.html','./research.html','./team.html','./publications.html','./join.html','./contact.html','./news.html',
   './en/','./en/index.html','./en/about.html','./en/research.html','./en/team.html','./en/publications.html','./en/join.html','./en/contact.html',
