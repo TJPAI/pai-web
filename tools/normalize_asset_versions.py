5a53dfd0dc6e9ae73df0dce91aecf0e6352ed391
TJPAI <erwu.liu@gmail.com>
2026-09-26 18:37:04 +0800

    Show sole partner summaries on desktop as well as mobile

--- tools/normalize_asset_versions.py
+++ tools/normalize_asset_versions.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import re, sys
 
 ROOT = Path(__file__).resolve().parents[1]
-REFINE_VERSION = "20260926-04"
+REFINE_VERSION = "20260926-05"
 SITE_JS_VERSION = "20260926-07"
 
 refine_pattern = re.compile(r"refine\.css\?v=[0-9-]+")
